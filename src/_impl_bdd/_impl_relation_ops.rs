4959933ab4674e6633ff61f2650ed1665f597680
Samuel Pastva <sam.pastva@gmail.com>
2020-10-13 12:05:43 +0200

    implement extended witness

--- src/_impl_bdd/_impl_relation_ops.rs
+++ src/_impl_bdd/_impl_relation_ops.rs
@@ -1,5 +1,6 @@
-use crate::{Bdd, BddNode, BddPointer};
+use crate::{Bdd, BddNode, BddPointer, BddVariable};
 use fxhash::FxBuildHasher;
+use std::cmp::max;
 use std::collections::HashMap;
 
 /// Advanced relation-like operations for `Bdd`s. Currently **experimental/unstable**.
@@ -90,78 +91,165 @@ impl Bdd {
     /// Currently, we only support a variable prefix because this makes the operation almost trivial
     /// to implement. In the future, we should aim to support this operation for an arbitrary
     /// subset of variables.
-    pub fn extended_witness(&self, _var_count: usize) -> Bdd {
-        todo!("Maybe skip for now?")
-        /*if var_count == 0 { return self.clone() };
-        /*
-            The algorithm "cuts" the Bdd into two parts - first var_count layers which will remain
-            the same and the remaining layers which will be pruned. From each node on the
-            var_count+1-st layer, select one (arbitrary) path by marking the nodes. If the
-            path meets an already marked node, just follow the already marked path (avoid
-            unnecessary markings). The new BDD then contains only the marked nodes in the second part.
-
-            This will ensure every valuation in the first layer will eventually lead to a single
-            path in the second layer. However, this approach technically does not minimise the
-            number of resulting nodes, however, is obviously always linear.
-
-            Also, remember that we have to actually fix "undecided" nodes (i.e. skipped variables).
-
-            Here, we actually perform marking and creation of new Bdd at the same time to save some
-            memory and time...
-         */
+    pub fn extended_witness(&self, var_count: u16) -> Bdd {
+        if self.is_false() {
+            return self.clone();
+        }
 
         let mut result = Bdd::mk_true(self.num_vars());
-        // Maps the old BddPointer to the index in the new Bdd
-        let mut mapping: HashMap<BddPointer, BddPointer> = HashMap::new();
 
-        // iterate the Bdd nodes (we have to process from bottom to top because otherwise copy
-        // would not be valid)
-        for i in self.pointers() {
-            if self.var_of(i) < var_count {
-                // We are in the first layer - just copy the node
-                let low = self.low_link_of(i);
-                let high = self.high_link_of(i);
-                // See that mapping[low]/mapping[high] must exist because low/high is already processed.
-                result.push_node(BddNode::mk_node(self.var_of(i), mapping[low], mapping[high]));
-                mapping.insert(i, result.root_pointer());
-            } else if self.var_of(i) == var_count {    // only mark paths from boundary layer
-                if mapping.contains_key(&i) { continue; }   // this could sometimes happen
-                // First, find path from boundary node to terminal or marked node
-                let mut path = Vec::new();
-                path.push(i);
-                while let Some(node) = path.last() {
-                    let low = self.low_link_of(*node);
-                    let high = self.high_link_of(*node);
-                    let to_add = if !low.is_zero() { low } else { high };
-                    if to_add.is_terminal() || mapping.contains_key(&node) { break; }
-                    path.push(to_add);
+        // Ensures we don't create duplicates.
+        let mut existing: HashMap<BddNode, BddPointer, FxBuildHasher> =
+            HashMap::with_capacity_and_hasher(self.size(), FxBuildHasher::default());
+        existing.insert(BddNode::mk_zero(self.num_vars()), BddPointer::zero());
+        existing.insert(BddNode::mk_one(self.num_vars()), BddPointer::one());
+
+        // Maps pointers in the old Bdd to pointers in the new Bdd.
+        let mut finished: HashMap<BddPointer, BddPointer, FxBuildHasher> =
+            HashMap::with_capacity_and_hasher(self.size(), FxBuildHasher::default());
+        // Terminals automatically map to each other - this ensures terminal never enters stack.
+        finished.insert(BddPointer::one(), BddPointer::one());
+        finished.insert(BddPointer::zero(), BddPointer::zero());
+
+        let mut stack = Vec::new();
+        stack.push(self.root_pointer());
+
+        /// Add extra Bdd nodes above new_node to ensure all variables up to the one in
+        /// on_stack is fixed. Return a pointer to the "new" new_node that has all variables
+        /// fixed.
+        fn fix_free_variables(
+            last_var_to_fix: u16,
+            new_node: BddPointer,
+            output: &mut Bdd,
+            existing: &mut HashMap<BddNode, BddPointer, FxBuildHasher>,
+        ) -> BddPointer {
+            /* TODO
+                There seems to be a better approach at picking values to fix.
+                The idea is to use existing nodes in the Bdd whenever possible. So instead of arbitrarily
+                always fixing to zero, check if the node has a parent with the correct variable and if so,
+                use that parent instead of creating an entire new node. But this requires computing predecessors...
+            */
+            if new_node.is_zero() {
+                return new_node;
+            } // zero needs no fixing
+            let mut fix_var = output.var_of(new_node).0;
+            if fix_var == 0 {
+                return new_node;
+            } // nothing to fix, but shouldn't happen...
+            fix_var -= 1; // move to the actual variable to fix
+            let mut result = new_node;
+            while fix_var >= last_var_to_fix {
+                // fix `fix_var` to zero
+                let node = BddNode::mk_node(BddVariable(fix_var), result, BddPointer::zero());
+                if let Some(&index) = existing.get(&node) {
+                    result = index;
+                } else {
+                    output.push_node(node);
+                    existing.insert(node, output.root_pointer());
+                    result = output.root_pointer();
                 }
-                // Mark path
-                let mut node = i;
-                while !node.is_terminal() {
-                    if marked.contains(&node) { break; }    // if already marked, path is finished
-                    marked.insert(node);
-                    let low = self.low_link_of(node);
-                    let high = self.high_link_of(node);
-                    if !low.is_zero() {
-                        node = low;
+                if last_var_to_fix == 0 && fix_var == 0 {
+                    break;
+                } // hack to handle 0 in last_var_to_fix
+                fix_var -= 1;
+            }
+            return result;
+        }
+
+        while let Some(&on_stack) = stack.last() {
+            if finished.contains_key(&on_stack) {
+                stack.pop();
+            } else if self.var_of(on_stack).0 < var_count {
+                // This node is in the upper layer - that means we need to process both children
+                // of the node normally (we might need to fix some values on the boundary though)
+                let (l, h) = (self.low_link_of(on_stack), self.high_link_of(on_stack));
+                let new_low = finished.get(&l).cloned();
+                let new_high = finished.get(&h).cloned();
+                if let (Some(new_low), Some(new_high)) = (new_low, new_high) {
+                    // Both children are resolved - resolve parent as well
+                    if new_low == new_high {
+                        // no decision, just propagate either node
+                        // (no fixing here because left and right would fix the same way, so we do it on actual decision)
+                        finished.insert(on_stack, new_low);
                     } else {
-                        node = high;
+                        // There is a decision here.
+                        // In case some variables need fixing...
+                        let last_var_to_fix = max(var_count, self.var_of(on_stack).0 + 1);
+                        let new_low = fix_free_variables(
+                            last_var_to_fix,
+                            new_low,
+                            &mut result,
+                            &mut existing,
+                        );
+                        let new_high = fix_free_variables(
+                            last_var_to_fix,
+                            new_high,
+                            &mut result,
+                            &mut existing,
+                        );
+                        let node = BddNode::mk_node(self.var_of(on_stack), new_low, new_high);
+                        if let Some(&index) = existing.get(&node) {
+                            // Node already exists, just make it a result of this computation.
+                            finished.insert(on_stack, index);
+                        } else {
+                            // Node does not exist, it needs to be pushed to result.
+                            result.push_node(node);
+                            existing.insert(node, result.root_pointer());
+                            finished.insert(on_stack, result.root_pointer());
+                        }
+                    }
+                } else {
+                    // If child is not resolved, push it and continue.
+                    if new_low.is_none() {
+                        stack.push(l);
+                    }
+                    if new_high.is_none() {
+                        stack.push(h);
                     }
                 }
-                assert!(node.is_one());
             } else {
-                // Non-boundary nodes are either processed inside the while loop above, or skipped
-                // entirely.
+                // This is the lower layer - here, we only need to process one child that
+                // corresponds to the path we are taking.
+
+                /* TODO: A better greedy strategy would prefer a finished node instead of picking left always. */
+                // Pick one child that has a valid path
+                let follow_in = if self.low_link_of(on_stack).is_zero() {
+                    self.high_link_of(on_stack)
+                } else {
+                    self.low_link_of(on_stack)
+                };
+
+                if let Some(follow_in) = finished.get(&follow_in).cloned() {
+                    let last_var_to_fix = max(var_count, self.var_of(on_stack).0 + 1);
+                    let follow_in =
+                        fix_free_variables(last_var_to_fix, follow_in, &mut result, &mut existing);
+                    // Make a new node, erasing one branch of the graph.
+                    let node = if self.low_link_of(on_stack).is_zero() {
+                        BddNode::mk_node(self.var_of(on_stack), BddPointer::zero(), follow_in)
+                    } else {
+                        BddNode::mk_node(self.var_of(on_stack), follow_in, BddPointer::zero())
+                    };
+                    if let Some(&index) = existing.get(&node) {
+                        // Node already exists, just make it a result of this computation.
+                        finished.insert(on_stack, index);
+                    } else {
+                        // Node does not exist, it needs to be pushed to result.
+                        result.push_node(node);
+                        existing.insert(node, result.root_pointer());
+                        finished.insert(on_stack, result.root_pointer());
+                    }
+                } else {
+                    stack.push(follow_in);
+                }
             }
         }
 
-        /*
-            Alternative algorithm to consider in the future: Reverse the edges in the lower layer
-            and compute a DFS tree starting from `1` node. Potentially simpler, but requires reversed
-            edges...
-         */
+        // At this point, the root pointer might still need fixing - although this should happen
+        // only when var_count == 0
+        if result.var_of(result.root_pointer()).0 > var_count {
+            fix_free_variables(var_count, result.root_pointer(), &mut result, &mut existing);
+        }
 
-        return ...*/
+        return result;
     }
 }
--- src/_test_bdd/_test_bdd_relation_ops.rs
+++ src/_test_bdd/_test_bdd_relation_ops.rs
@@ -35,3 +35,46 @@ fn bdd_projection_simple() {
         assert_eq!(variables.mk_true(), bdd.projection(1));
     }
 }
+
+#[test]
+fn bdd_extended_witness_trivial() {
+    let variables = mk_5_variable_set();
+    let bdd = mk_small_test_bdd(); // v_3 && !v_4
+    let tt = variables.mk_true();
+    let ff = variables.mk_false();
+
+    assert_eq!(ff, ff.extended_witness(3));
+    assert_eq!(ff, ff.extended_witness(0));
+
+    assert_eq!(tt, tt.extended_witness(5));
+    let expected = variables.eval_expression_string("!v1 & !v2 & !v3 & !v4 & !v5");
+    assert_eq!(expected, tt.extended_witness(0));
+    let expected = variables.eval_expression_string("!v4 & !v5");
+    assert_eq!(expected, tt.extended_witness(3));
+
+    assert_eq!(bdd, bdd.extended_witness(5));
+    let expected = variables.eval_expression_string("!v1 & !v2 & v3 & !v4 & !v5");
+    assert_eq!(expected, bdd.extended_witness(0));
+    let expected = variables.eval_expression_string("v3 & !v4 & !v5");
+    assert_eq!(expected, bdd.extended_witness(3));
+}
+
+#[test]
+fn bdd_extended_witness_simple() {
+    let variables = mk_5_variable_set();
+    let bdd = variables.eval_expression_string("(v1 => (v4 <=> v5)) & (!v1 => !(v4 <=> v5))");
+    let expected = variables.eval_expression_string("(v1 => (!v4 & !v5)) & (!v1 => (!v4 & v5))");
+    assert_eq!(expected, bdd.extended_witness(3));
+    assert_eq!(bdd, bdd.extended_witness(4));
+
+    let bdd = variables.eval_expression_string("(v1 <=> v5) & (v2 => v4) & (v3 ^ v2)");
+    //println!("{}", bdd.to_dot_string(&variables, true));
+    let expected = variables
+        .eval_expression_string("(v1 => (!v2 & v3 & !v4 & v5)) & (!v1 => (!v2 & v3 & !v4 & !v5))");
+    assert_eq!(expected, bdd.extended_witness(1));
+    let expected = variables.eval_expression_string("((v1 & v2) => (!v3 & v4 & v5)) & ((v1 & !v2) => (v3 & !v4 & v5)) & ((!v1 & v2) => (!v3 & v4 & !v5)) & ((!v1 & !v2) => (v3 & !v4 & !v5))");
+    assert_eq!(expected, bdd.extended_witness(2));
+    assert_eq!(expected, bdd.extended_witness(3)); // accidentally, this works out
+    assert_eq!(bdd, bdd.extended_witness(4));
+    assert_eq!(bdd, bdd.extended_witness(5));
+}
