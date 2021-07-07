20f4dee9dd037ce29776f3c7ef650a94ebd5ecc6
Samuel Pastva <daemontus@gmail.com>
2021-07-07 12:22:00 +0200

    A functioning itgr-based benchmark.

--- benches/itgr.rs
+++ benches/itgr.rs
@@ -1,33 +1,40 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Bencher};
-use biodivine_lib_bdd::{bdd, BddVariableSet};
-
-fn ripple_carry_adder(b: &mut Bencher, num_vars: u16) {
-    let vars = BddVariableSet::new_anonymous(num_vars);
-    let variables = vars.variables();
-    b.iter(|| {
-        let mut result = vars.mk_false();
-        for x in 0..(num_vars / 2) {
-            let x1 = vars.mk_var(variables[x as usize]);
-            let x2 = vars.mk_var(variables[(x + num_vars / 2) as usize]);
-            result = bdd!(result | (x1 & x2));
-        }
-        result
-    });
-}
+use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
+use biodivine_lib_bdd::Bdd;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("ripple-carry-adder 4", |b| {
-        ripple_carry_adder(b, 4);
+    let mut benchmarks = Vec::new();
+    for file in std::fs::read_dir("./bench_inputs/itgr").unwrap() {
+        let file = file.unwrap();
+        let path = file.path();
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        if file_name.ends_with(".and_not.left.bdd") {
+            let bench_name = &file_name[..(file_name.len() - 17)];
+            benchmarks.push(bench_name.to_string());
+        }
+    }
+    // Actually do the benchmarks in some sensible order.
+    benchmarks.sort_by_cached_key(|name| {
+        let mut split = name.split(".");
+        split.next();
+        let size = split.next().unwrap();
+        size.parse::<usize>().unwrap()
     });
-    c.bench_function("ripple-carry-adder 8", |b| {
-        ripple_carry_adder(b, 8);
-    });
-    c.bench_function("ripple-carry-adder 16", |b| {
-        ripple_carry_adder(b, 16);
-    });
-    c.bench_function("ripple-carry-adder 32", |b| {
-        ripple_carry_adder(b, 32);
-    });
+
+    let mut group = c.benchmark_group("itgr");
+    // Some of the benchmarks will run for a long time, so this setting is recommended.
+    group.sampling_mode(SamplingMode::Flat);
+    for benchmark in &benchmarks {
+        let left_path = format!("./bench_inputs/itgr/{}.and_not.left.bdd", benchmark);
+        let left = Bdd::from_string(std::fs::read_to_string(&left_path).unwrap().as_str());
+        let right_path = format!("./bench_inputs/itgr/{}.and_not.right.bdd", benchmark);
+        let right = Bdd::from_string(std::fs::read_to_string(right_path).unwrap().as_str());
+        group.bench_function(benchmark, |b| {
+            b.iter(|| {
+                left.and_not(&right)
+            });
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
