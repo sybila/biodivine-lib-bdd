1cd1cc3b15d4ae724ad701f2cf9f5c51b88dbbfe
Samuel Pastva <daemontus@gmail.com>
2021-08-31 21:28:38 +0200

    Add most positive/negative valuation methods.

--- src/_impl_bdd/_impl_valuation_utils.rs
+++ src/_impl_bdd/_impl_valuation_utils.rs
@@ -2,7 +2,7 @@ use crate::{Bdd, BddPartialValuation, BddValuation};
 
 /// Utilities for extracting interesting valuations and paths from a `Bdd`.
 impl Bdd {
-    /// Return the lexicographically smallest satisfying valuation of this `Bdd`.
+    /// Return the lexicographically first satisfying valuation of this `Bdd`.
     ///
     /// (In this context, lexicographically means `0 < 1`, with greatest variable id
     /// being the most significant).
@@ -25,7 +25,7 @@ impl Bdd {
         Some(valuation)
     }
 
-    /// Return the lexicographically largest satisfying valuation of this `Bdd`.
+    /// Return the lexicographically last satisfying valuation of this `Bdd`.
     ///
     /// (In this context, lexicographically means `0 < 1`, with greatest variable id
     /// being the most significant).
@@ -48,7 +48,7 @@ impl Bdd {
         Some(valuation)
     }
 
-    /// Return the lexicographically smallest path in this `Bdd`, represented as
+    /// Return the lexicographically fist path in this `Bdd`, represented as
     /// a *conjunctive* clause.
     pub fn first_path(&self) -> Option<BddPartialValuation> {
         if self.is_false() {
@@ -70,7 +70,7 @@ impl Bdd {
         Some(valuation)
     }
 
-    /// Return the lexicographically largest path in this `Bdd`, represented as
+    /// Return the lexicographically last path in this `Bdd`, represented as
     /// a *conjunctive* clause.
     pub fn last_path(&self) -> Option<BddPartialValuation> {
         if self.is_false() {
@@ -90,6 +90,114 @@ impl Bdd {
             } else {
                 valuation.set_value(self.var_of(node), true);
                 node = self.high_link_of(node);
+            }
+        }
+
+        Some(valuation)
+    }
+
+    /// Return a valuation in this `Bdd` that contains the greatest amount of positive literals.
+    ///
+    /// If such valuation is not unique, the method should return the one that is first
+    /// lexicographically.
+    pub fn most_positive_valuation(&self) -> Option<BddValuation> {
+        if self.is_false() {
+            return None;
+        }
+
+        let mut cache = Vec::with_capacity(self.size());
+        cache.push((0, true));
+        cache.push((0, true));
+
+        for i in self.pointers().skip(2) {
+            let i_var = self.var_of(i);
+            let low_link = self.low_link_of(i);
+            let high_link = self.high_link_of(i);
+
+            // Parenthesis to avoid a chance of overflow.
+            let low_link_diff =
+                cache[low_link.to_index()].0 + ((self.var_of(low_link).0 - i_var.0) - 1);
+            let high_link_diff =
+                cache[high_link.to_index()].0 + ((self.var_of(high_link).0 - i_var.0) - 1);
+
+            let result = if low_link.is_zero() && high_link.is_zero() {
+                panic!("Non canonical BDD.")
+            } else if low_link.is_zero() {
+                (high_link_diff + 1, true)
+            } else if high_link.is_zero() {
+                (low_link_diff, false)
+            } else if high_link_diff + 1 > low_link_diff {
+                (high_link_diff + 1, true)
+            } else {
+                (low_link_diff, false)
+            };
+
+            cache.push(result);
+        }
+
+        let mut valuation = BddValuation::all_true(self.num_vars());
+        let mut node = self.root_pointer();
+        while !node.is_terminal() {
+            let (_, child) = cache[node.to_index()];
+            if child {
+                node = self.high_link_of(node);
+            } else {
+                valuation.clear(self.var_of(node));
+                node = self.low_link_of(node);
+            }
+        }
+
+        Some(valuation)
+    }
+
+    /// Return a valuation in this `Bdd` that contains the greatest amount of negative literals.
+    ///
+    /// If such valuation is not unique, the method should return the one that is first
+    /// lexicographically.
+    pub fn most_negative_valuation(&self) -> Option<BddValuation> {
+        if self.is_false() {
+            return None;
+        }
+
+        let mut cache = Vec::with_capacity(self.size());
+        cache.push((0, true));
+        cache.push((0, true));
+
+        for i in self.pointers().skip(2) {
+            let i_var = self.var_of(i);
+            let low_link = self.low_link_of(i);
+            let high_link = self.high_link_of(i);
+
+            // Parenthesis to avoid a chance of overflow.
+            let low_link_diff =
+                cache[low_link.to_index()].0 + ((self.var_of(low_link).0 - i_var.0) - 1);
+            let high_link_diff =
+                cache[high_link.to_index()].0 + ((self.var_of(high_link).0 - i_var.0) - 1);
+
+            let result = if low_link.is_zero() && high_link.is_zero() {
+                panic!("Non canonical BDD.")
+            } else if low_link.is_zero() {
+                (high_link_diff, true)
+            } else if high_link.is_zero() {
+                (low_link_diff + 1, false)
+            } else if high_link_diff > low_link_diff + 1 {
+                (high_link_diff, true)
+            } else {
+                (low_link_diff + 1, false)
+            };
+
+            cache.push(result);
+        }
+
+        let mut valuation = BddValuation::all_false(self.num_vars());
+        let mut node = self.root_pointer();
+        while !node.is_terminal() {
+            let (_, child) = cache[node.to_index()];
+            if child {
+                valuation.set(self.var_of(node));
+                node = self.high_link_of(node);
+            } else {
+                node = self.low_link_of(node);
             }
         }
 
@@ -131,5 +239,17 @@ mod tests {
 
         assert_eq!(Some(first_path), bdd.first_path());
         assert_eq!(Some(last_path), bdd.last_path());
+    }
+
+    #[test]
+    fn most_positive_negative_valuation() {
+        let vars = BddVariableSet::new_anonymous(5);
+        let bdd = vars.eval_expression_string("x_0 & (!x_2 | x_3) & !x_4");
+
+        let most_positive_valuation = BddValuation(vec![true, true, true, true, false]);
+        let most_negative_valuation = BddValuation(vec![true, false, false, false, false]);
+
+        assert_eq!(Some(most_positive_valuation), bdd.most_positive_valuation());
+        assert_eq!(Some(most_negative_valuation), bdd.most_negative_valuation());
     }
 }
