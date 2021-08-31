d2ac6d744907faac1900a21b19e8faa2556f97a2
Samuel Pastva <daemontus@gmail.com>
2021-08-31 22:26:00 +0200

    Add tutorial module about valuations.

--- src/tutorial/mod.rs
+++ src/tutorial/mod.rs
@@ -6,9 +6,11 @@
 //!  - [Creating a `BddVariableSet` and `BddVariable`-s](./p02_bdd_variable_set/index.html)
 //!  - [Manipulating `Bdd`s idiomatically](./p03_bdd_manipulation/index.html)
 //!  - [Serialising and visualising `Bdd`s](./p04_bdd_serialisation/index.html)
+//!  - [Working with BDD valuations and paths](./p05_bdd_valuations/index.html)
 //!
 
 pub mod p01_bdd_intro;
 pub mod p02_bdd_variable_set;
 pub mod p03_bdd_manipulation;
 pub mod p04_bdd_serialisation;
+pub mod p05_bdd_valuations;
